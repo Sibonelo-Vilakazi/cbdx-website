c248f894b6671a09dcdd68f33652612c2b8eacb4
Sibonelo-Vilakazi <sibonelo@monkeyandriver.com>
2022-02-09 20:34:48 +0200

    adding the pages and the json object

---
+++ src/app/pages/products/products.component.ts
@@ -0,0 +1,19 @@
+import { ProductsService } from './../../services/products.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+
+  constructor(private productsService:ProductsService) { }
+
+  ngOnInit(): void {
+    this.productsService.getProducts('products').subscribe((data :any )=>{
+      console.log(data)
+    })
+  }
+
+}
