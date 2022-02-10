9ee4e34a5ff9f494aa66961875073a2cc7650641
Sibonelo-Vilakazi <sibonelo@monkeyandriver.com>
2022-02-10 21:23:00 +0200

    Removing files I dont need for te video

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductsComponent } from './pages/products/products.component';
 
-const routes: Routes = [
-  {
-    path:'products' , component :ProductsComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
-import { ProductsComponent } from './pages/products/products.component';
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { CategoryTabComponent } from './components/category-tab/category-tab.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent,
-    CategoryTabComponent
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
